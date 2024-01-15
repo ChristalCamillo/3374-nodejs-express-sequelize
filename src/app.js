cd3af3d358333e5d4d07171a518f9c7b23d70c4c
ChristalCamillo <christal.b.camillo@gmail.com>
2024-01-15 18:02:41 -0300

    aula3

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+// Por que estou usando CommonJS e não import-export? 
+// Porque algumas ferramentas específicas do Sequelize, como a ferramenta de linha de comando que utilizaremos neste curso, funcionam melhor com CommonJS e ainda não se adaptaram tão bem ao import-export.
+// Também é para mantermos uma compatibilidade com a documentação, que por enquanto está toda usando CommonJS.
+
 const express = require('express');
 
 const app = express();
